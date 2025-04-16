8809581dbe196ba690dcc9d275eefc90dc73ecb9
QuiRohn <QuiRohn02@gmail.com>
2025-04-16 14:52:02 +0200

    Initial Commit

---
+++ draft/draftApp.swift
@@ -0,0 +1,17 @@
+//
+//  draftApp.swift
+//  draft
+//
+//  Created by Rocco Geremia Ciccone on 16/04/25.
+//
+
+import SwiftUI
+
+@main
+struct draftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
